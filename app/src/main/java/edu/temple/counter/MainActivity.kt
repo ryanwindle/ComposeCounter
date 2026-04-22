1d0d3d3aa61ead7d11f2502097dbc8d619b3e414
ryanwindle <tus27108@temple.edu>
2026-04-22 15:15:50 -0400

    Counter implemented with compose. (again)

--- app/src/main/java/edu/temple/counter/MainActivity.kt
+++ app/src/main/java/edu/temple/counter/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,13 +44,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
-    Column (modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            text = "0",
-            fontSize = 192.sp
-        )
+//    Column (modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally) {
+//        Text(
+//            text = "0",
+//            fontSize = 192.sp
+//        )
+//    }
+    var count by remember { mutableIntStateOf(0) }
+
+    Column (
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { count++ }) {
+            Image(painter = painterResource(android.R.drawable.arrow_up_float), "up arrow")
+        }
+        Text(text = count.toString(), fontSize = 192.sp)
+        Button(onClick = { count-- }) {
+            Image(painter = painterResource(android.R.drawable.arrow_down_float), "down arrow")
+        }
     }
+
 }
 
 @Preview(showBackground = true)
